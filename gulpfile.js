8386754aa7e3bc303adccd00e635fe3bf0c7d3ea
wuyihao1992 <840461198@qq.com>
2017-01-09 14:30:55 +0800

    gulp browser-sync 与 server.js冲突

--- gulpfile.js
+++ gulpfile.js
@@ -27,16 +27,16 @@ gulp.task('build', ['clean'], function(cb) {
     return gulp.start(['framework', 'sass'], cb);
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            //指定服务器启动根目录
-            baseDir: "./bin"
-        }
-    });
-    //监听任何文件变化，实时刷新页面
-    gulp.watch("./bin/**/*.*").on('change', browserSync.reload);
-});
+// gulp.task('browser-sync', function() {
+//     browserSync({
+//         server: {
+//             //指定服务器启动根目录
+//             baseDir: "./"
+//         }
+//     });
+//     //监听任何文件变化，实时刷新页面
+//     gulp.watch("./**/*.*").on('change', browserSync.reload);
+// });
 
 gulp.task('sass', function() {
     gulp.src([
